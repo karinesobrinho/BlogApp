c06a880fdba4a879bbbcd9145ffc122872a20bed
karine sobrinho <karine>
2020-06-17 14:39:25 -0300

    aula 140

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+import {Context} from '../context/BlogContext'
+
+const CreateScreen = ({navigation}) => {
+    return(
+        <View>
+            <Text>Createeeee</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default CreateScreen
